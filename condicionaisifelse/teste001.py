89cbb14272a81b0ae4da0ccab361ce6691b3b45c
Vinizin7788* <vfontes076@gmail.com>
2024-03-19 14:56:08 -0400

    nova estrutura de pasta

--- condicionaisifelse/teste001.py
+++ condicionaisifelse/teste001.py
@@ -36,11 +36,11 @@ else :
     print("Sua letra é uma consoante! ")
 
 #Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
-
+"""
 A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
 A mensagem "Reprovado", se a média for menor do que sete;
 A mensagem "Aprovado com Distinção", se a média for igual a dez.
-
+"""
 
 n1 = float(input("Qual é a sua nota de VGA Paulo? "))    
 n2 = float(input("Qual é a sua nota de Cálculo I Paulo? "))
