7995070b3a0f6ad090c4ff5396a8a0fb63f4bb4f
Vinizin7788 <vfontes076@gmail.com>
2024-03-18 21:19:15 -0400

    Exercicio 2

---
+++ teste008.py
@@ -0,0 +1,6 @@
+numeroum = int(input("Escolha um valor: "))
+dobro = numeroum * 2
+triplo = numeroum * 3
+raizquadrada = int(numeroum ** 0.5)
+print("O dobro do número " + str(numeroum) + " é " + str(dobro) + " já o seu triplo é " + str(triplo) + " e sua raiz quadrada é " + str(raizquadrada))
+
