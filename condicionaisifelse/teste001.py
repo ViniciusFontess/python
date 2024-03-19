45d0b8b3a46088c00ea59e33861422ae57f57891
Vinizin7788 <vfontes076@gmail.com>
2024-03-19 13:16:22 -0400

    teste001.py

---
+++ condicionaisifelse/teste001.py
@@ -0,0 +1,7 @@
+cor = input("qual é a cor do sinal agora?")
+if cor == "verde":
+    print("Acelere")
+elif cor == "amarelo" :
+    print("Atenção")
+else cor == "vermelho" :
+    print("Não passe")
