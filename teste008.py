ce040f03786952db36cf08cc52dc3cfc1431ae09
Vinizin7788 <vfontes076@gmail.com>
2024-03-21 19:47:53 -0400

    html

--- teste008.py
+++
@@ -1,6 +0,0 @@
-numeroum = int(input("Escolha um valor: "))
-dobro = numeroum * 2
-triplo = numeroum * 3
-raizquadrada = int(numeroum ** 0.5)
-print("O dobro do número " + str(numeroum) + " é " + str(dobro) + " já o seu triplo é " + str(triplo) + " e sua raiz quadrada é " + str(raizquadrada))
-
