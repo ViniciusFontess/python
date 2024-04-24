7992286b8bd80db4c09181910f7c73ead29540d2
Vinizin7788 <vfontes076@gmail.com>
2024-04-24 09:15:24 -0400

    att

---
+++ algprog3/while.py
@@ -0,0 +1,16 @@
+"""n= int(input()) 
+i=0  
+numero =1
+while i<n: 
+    print(numero)
+
+    numero = numero + 2
+    i= i+1"""
+
+i = int(input())
+t=1
+while(i>0):
+    t = t*i
+
+    i=i-1
+print(t)    
---
+++ algprog3/whilelista.py
@@ -0,0 +1,8 @@
+n = int(input()) 
+i = 0
+while (i<n) :
+    print(2**i)
+
+
+
+    i=i+1
