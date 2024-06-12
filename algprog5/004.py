ef9bae4a0e969c9bb0135d351421ce2be052d643
Vinicius <163762205+Vinizin7788@users.noreply.github.com>
2024-06-12 09:14:37 -0400

    Create 004.py

---
+++ algprog5/004.py
@@ -0,0 +1,29 @@
+n = int(input())
+i = 0
+vetor = []
+contador = 0
+contador1 = 0
+contador2 = 0
+contador3 = 0
+contador4 = 0
+contador5 = 0
+while i < n :
+    resultado_lancamento = input()
+    i+=1
+    vetor.append(resultado_lancamento)
+    j=0
+while j < len(vetor)  :
+        if vetor[j] == '1' :
+            contador = contador + 1
+        if vetor[j] == '2' :
+            contador1 = contador1 + 1
+        if vetor[j] == '3' :
+            contador2 = contador2 + 1
+        if vetor[j] == '4' :
+            contador3 = contador3 + 1
+        if vetor[j] == '5' :
+            contador4 = contador4 + 1
+        if vetor[j] == '6' :
+            contador5 =  contador5 + 1 
+        j+=1          
+print(contador,contador1,contador2,contador3,contador4,contador5)
