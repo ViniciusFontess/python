63886a424b4aad081176bd205aff306391ff1a89
Vinicius <163762205+Vinizin7788@users.noreply.github.com>
2024-06-27 17:26:28 -0400

    Update sistema_matricula.py

--- algprog5/sistema_matricula.py
+++ algprog5/sistema_matricula.py
@@ -15,7 +15,7 @@
 
 #REMOVER ALUNO
 
-"""def remover(cpf):
+def remover(cpf):
     i = 0
     while i < len(ler_linha0): 
         mensagem0 = ler_linha0[i].split("-")
@@ -29,7 +29,7 @@
     i+=1       
     data1 = open("matricula.txt" , "w")
     data1.write(dados)    
-    data1.close()"""
+    data1.close()
 
 #EDITAR ALUNO 
 """def editar_aluno"""
@@ -49,6 +49,7 @@ while continua:
     print("2- Cadastrar matrícula, Editar matrícula, Remover matrícula ou Listar Disciplinas")
     print("3- Matricular aluno em uma Disciplina")
     print("4- Cancelar Matrícula de um estudante")
+
     borda()
     o_que_deseja_fazer = int(input())
     if o_que_deseja_fazer == 1:
@@ -57,6 +58,7 @@ while continua:
         print("2- Editar aluno")
         print("3- Remover aluno")
         print("4- Listar alunos")
+
         borda()
         o_que_deseja_fazer2 = int(input())
 
@@ -68,29 +70,43 @@ while continua:
             print("Digite o nome do aluno: ")
             cadastrar_nome_aluno = input()
             print("Digite o CPF do aluno: ")
-            cpf_aluno = str(input())
-            if len(cpf_aluno) == 11 : 
-                continua = True
-            else :
-                print("Digite seu CPF corretamente e digite apenas números! ")
-                continua = False    
+            cpf_aluno = str(input())   
             data = open("matricula.txt" , "a") 
             mensagem = cadastrar_nome_aluno + " - " + str(cpf_aluno)
             data.write(mensagem)
             data.close()
             data1 = open("matricula.txt" , "r")
             ler_linha = data1.readlines()
-
+            data1.close()
             #Enumerar os alunos da lista
             i = 0
-            while i < len(ler_linha): 
-                mensagem1 = ler_linha[0].split("")
-                print(str(i) + "- " + str(mensagem1[0]))
+            while i < len(ler_linha):
+                mensagem = ler_linha[i].split("-")
+                print(str(i) + "- " + str(mensagem[0] + "- " + cpf_aluno))
                 i+=1
 
             #Editar aluno, Nome e CPF
 
-        if o_que_deseja_fazer == 4:
+        if o_que_deseja_fazer2 == 3:
+            print("Você deseja remover qual aluno? Digite o índice: ")
+            data2 = open("matricula.txt" , "r")
+            printar_usuario = data2.write()
+            data2.close
+            print(printar_usuario)
+            indice = int(input())
+            print(printar_usuario[indice])
+            print("Tem certeza que deseja removê-lo?")
+            print("1- SIM")
+            print("2- NÃO")
+            sim_nao = input()
+            if sim_nao == "SIM":
+                
+
+                
+                chamar = remover()
+        
+
+        if o_que_deseja_fazer2 == 4:
             lista_alunos = open("matricula.txt" , "r")
             print(lista_alunos)
             lista_alunos.close()
